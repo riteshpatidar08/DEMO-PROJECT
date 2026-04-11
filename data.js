8f2867d78ff3e87c7bc47a31328d93189ad70234
ritesh <riteshpatidar088@gmail.com>
2026-04-11 16:15:00 +0530

    second commit

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 console.log('my name is ritesh') ;
-
+console.log('hell this the second commit')
 
 // git config --global user.name "ritesh" 
 // git config --global user.email "riteshpatidar@gmail" 
