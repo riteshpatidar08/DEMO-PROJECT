24767e97cc7a2c5d30e9e7595294aba08517de09
ritesh <riteshpatidar088@gmail.com>
2026-04-13 17:38:21 +0530

    email functionality

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ console.log('hi') ;
 console.log('hellooo');
 console.log('addToCart');
 console.log('product added to cart')
+console.log('f')
 
-console.log('payment prcessing...')
+console.log('payment prcessing...') 
+
+
+console.log('email notification')
